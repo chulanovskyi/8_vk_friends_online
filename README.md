7ac8cdb4fb86b056fdf5593dfe5a0631f086f506
chulanovskyi <skyrocker@mail.ru>
2016-09-30 09:53:23 +0300

    it works!

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 8_vk_friends_online
+
+###Prerequisites:
+
+Run in console `pip install -r requirements.txt` to install 3rd party modules.
+
+---
+
+This script simply displays who's of your friends are currently online in vk.com
+
+###How to use:
+
+To be able to search, script will ask you for your credentials in vk.com (login/password)
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -17,14 +17,18 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope = 'friends',
     )
     api = vk.API(session)
-    friends_id_online = api.friends.online()
-    for friend in friends_id_online:
-        print(api.users.get(friend))
+    friends_id_online = api.friends.getOnline()
+    friends_online = api.users.get(user_ids=friends_id_online)
+    return friends_online
+
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Friends are currently online:')
+    for friend in friends_online:
+        print(friend['last_name'], friend['first_name'])
 
 
 if __name__ == '__main__':
@@ -32,4 +36,3 @@ if __name__ == '__main__':
     password = get_user_password()
     friends_online = get_online_friends(login, password)
     output_friends_to_console(friends_online)
-
